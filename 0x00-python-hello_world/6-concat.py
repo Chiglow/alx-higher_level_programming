831392fedce708b9f7601b0c385d45f982cd6856
Chiglow <chidinmagloria79@yahoo.com>
2022-11-30 00:33:24 -0800

    laughter

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
-str2 = "school"
-str1 = str1 + ' ' + str2
-print(F"Welcome {}!" .format(str))
+str2 = "School"
+str1 += (" " + str2)
+print("Welcome to {}!".format(str1))
